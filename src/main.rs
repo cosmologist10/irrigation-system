aaac09ee068833bb69ad17ca105e6663cfe80c3d
cosmologist10 <sumanshweta44@gmail.com>
2022-10-30 01:16:11 +0530

    debugged rocket error from rocket main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use irrigation;
 use rocket;
 
+
 #[rocket::main]
 async fn main() -> Result<(), rocket::Error> {
-    irrigation::rocket().launch().await
+    irrigation::rocket().launch().await?;
+    Ok(())
 }
