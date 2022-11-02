12611992a943c855c8445cd0b2b828e6969bfd67
cosmologist10 <sumanshweta44@gmail.com>
2022-11-03 01:52:10 +0530

    added user and database creation sql commands

--- init.sql
+++ init.sql
@@ -1,3 +1,4 @@
-CREATE USER irrigation WITH PASSWORD 'irrigation';
-CREATE DATABASE irrigation;
-GRANT ALL PRIVILEGES ON DATABASE irrigation TO irrigation;
+create database irrigation;
+create user 'cosmo'@'localhost' identified with mysql_native_password by 'oldmonk@123';
+GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, INDEX, DROP, ALTER, CREATE TEMPORARY TABLES, LOCK TABLES ON irrigation.* TO 'cosmo'@'localhost';
+GRANT FILE ON *.* TO 'cosmo'@'localhost';
