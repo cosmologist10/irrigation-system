9587a8367c054190c0e3c0b6d29acbedb9d4d2df
shweta <shweta@shwetas-MacBook-Pro.local>
2022-11-26 19:48:21 +0530

    debugging Measurement API struct issue

--- src/routes/measurements.rs
+++ src/routes/measurements.rs
@@ -1,71 +1,77 @@
-use rocket::serde::json::{json, Json, Value};
+// use std::any;
+
+use rocket::serde::json::{json, Value};
+use chrono::{DateTime, Duration, Utc};
+
 use rocket::State;
 use serde::Deserialize;
 use chrono::prelude::*;
 
+#[derive(Deserialize)]
+struct MeasurementQueryFilter {
+    sensor_name: &'a str,
+    start: DateTime<Utc>,
+    end: DateTime<Utc>,
+}
 
 
 #[get("/all/<sensor_name>")]
-fn get_all_measurements(sensor_name: &str) -> String {
-    let query_filter = {
-        sensor_name
-    }
-    format!("Sensor name : {}!", sensor_name);
-    // return daily_measurements with sensor name
+fn get_all_measurements(sensor_name: &str) -> Value {
+    json!({ "Sensor name": sensor_name })
 }
 
-#[get("/month/<sensorName>")]
-fn get_last_month_measurements(sensor_name: &str) -> String {
-    let query_filter = {
-        sensor_name,
-        let start: DateTime<Utc> = Utc::now(),
-        let end: DateTime<Utc> = Utc::now() - Duration::month(1),
-    }
-    format!("Sensor name : {}!", query_filter);
+#[get("/month/<sensor_name>")]
+fn get_last_month_measurements(sensor_name:&str) -> Value {
+    let query_filter = MeasurementQueryFilter {
+        sensor_name: sensor_name,
+        start: Utc::now(),
+        end: Utc::now() - Duration::month(1)
+    };
+    json!({ "Sensor name": sensor_name })
     // return daily_measurements with sensor name,lte and gte
 }
 
 
-#[get("/week/<sensorName>")]
-fn get_last_week_measurements(sensor_name: &str) -> String {
-    let query_filter = {
-        sensor_name,
-        let start: DateTime<Utc> = Utc::now(),
-        let end: DateTime<Utc> = Utc::now() - Duration::days(7)
-    }
-    format!("Sensor name : {}!", sensor_name);
+#[get("/week/<sensor_name>")]
+fn get_last_week_measurements(sensor_name: &str) -> Value {
+    let query_filter = MeasurementQueryFilter{
+        sensor_name: sensor_name,
+        start: Utc::now(),
+        end: Utc::now() - Duration::days(7)
+    };
+    json!({ "Sensor name": sensor_name })
     // return getHourlyMeasurements with sensor name,lte and gte
 }
 
-#[get("/day/<sensorName>")]
-fn get_last_day_measurements(sensor_name: &str) -> String {
-    let query_filter = {
+#[get("/day/<sensor_name>")]
+fn get_last_day_measurements(sensor_name: &str) -> Value {
+    let query_filter = MeasurementQueryFilter{
         sensor_name,
-        let start: DateTime<Utc> = Utc::now(),
-        let end: DateTime<Utc> = Utc::now() - Duration::days(1)
-    }
-    format!("Sensor name : {}!", sensor_name);
+        start: Utc::now(),
+        end: Utc::now() - Duration::days(1)
+    };
+    json!({ "Sensor name": sensor_name })
     // return getHourlyMeasurements with sensor name,lte and gte
 }
 
-#[get("/hour/<sensorName>")]
-fn get_last_hour_measurements(sensor_name: &str) -> String {
-    let query_filter = {
+#[get("/hour/<sensor_name>")]
+fn get_last_hour_measurements(sensor_name: &str) -> Value {
+    let query_filter = MeasurementQueryFilter{
         sensor_name,
-        let start: DateTime<Utc> = Utc::now(),
-        let end: DateTime<Utc> = Utc::now() - Duration::hour(1)
-    }
-    format!("Sensor name : {}!", sensor_name);
+        start: Utc::now(),
+        end: Utc::now() - Duration::hour(1)
+    };
+    json!({ "Sensor name": sensor_name })
     // return getMinutelyMeasurements with sensor name,lte and gte
 }
 
-#[get("/minute/<sensorName>")]
-fn get_last_minute_measurements(sensor_name: &str) -> String {
-    let query_filter = {
+#[get("/minute/<sensor_name>")]
+fn get_last_minute_measurements(sensor_name: &str) -> Value {
+    let query_filter = MeasurementQueryFilter{
         sensor_name,
-        let start: DateTime<Utc> = Utc::now(),
-        let end: DateTime<Utc> = Utc::now() - Duration::hour(1)
-    }
-    format!("Sensor name : {}!", sensor_name);
+        start: Utc::now(),
+        end: Utc::now() - Duration::hour(1)
+    };
+    json!({ "Sensor name": sensor_name })
     // return getSecondlyMeasurements with sensor name,lte and gte
 }
